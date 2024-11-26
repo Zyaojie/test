a7dab3f41c8dcc8178fecf3bf596f1dea484f2dd
Zyaojie <1750724954@qq.com>
2024-11-26 20:57:01 +0800

    test

--- requests.demo.py
+++ requests.demo.py
@@ -1,1 +1,27 @@
 import requests
+url = 'http://127.0.0.1:8787//dar/user/login'
+
+header = {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+
+data = {
+    'user_name': 'test01',
+    'passwd': 'admin123'
+}
+
+#-------------------------post-----------------
+res = requests.post(url=url,data=data)
+
+print(res.text)
+
+#------------------------get--------------------------------
+
+url_2 = 'http://127.0.0.1:8787//coupApply/cms/goodsList'
+header_2 = {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+jsondata ={
+"msgType": "getHandsetListOfCust",
+"page":1,
+"size":20
+}
+
+res2 = requests.get(url=url_2,params=jsondata,headers=header_2)
+print(res2.text)
