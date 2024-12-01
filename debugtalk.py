e94fe87b110db076ba1886038d5132332b0a2074
Zyaojie <1750724954@qq.com>
2024-12-01 16:55:48 +0800

    读写yaml文件

---
+++ debugtalk.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/12/1 16:47
+# @Author : 17507
+# @File : debugtalk
+# @Project : api-test
+from readyaml import ReadyamlData
+
+class DebugTalk:
+
+    def __int__(self):
+        pass
+
+    def get_extract_data(self,node_name,random=None):
+        '''获取extract.yaml
+        :param node_name: extract.yamli中的key值
+        ：param'''
---
+++ readyaml.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/12/1 12:56
+# @Author : 17507
+# @File : readyaml
+# @Project : api-test
+import os
+
+import yaml
+
+
+def get_testcase_yaml(file):
+    '''
+    获取yaml文件的数据
+    :param file: yaml文件的路径
+    :return:
+    '''
+
+    try:
+        with open(file, 'r' ,encoding='utf-8') as f:
+            yaml_data = yaml.safe_load(f)
+            return yaml_data
+    except Exception as e:
+        print(f'读取yaml文件出错：{str(e)}')
+
+class ReadyamlData:
+    '''读取yaml数据，以及写入数据到yaml文件'''
+    def __init__(self, yaml_fine=None):
+        if yaml_fine is not  None:
+            self.yaml_fine = yaml_fine
+        else:
+            self.yaml_fine = 'login.yaml'
+
+    def write_yaml_data(self,value):
+        '''写入数据到yaml文件
+        ：param value: （dict）写入的数据
+        ：return：
+        '''
+        file = None
+        file_path = r'extract.yaml'
+        if not os.path.exists(file_path):
+            os.system(file_path)
+        try:
+            # file =  open(file_path,'a（a=追加读写，w=清空读写）',encoding='utf-8')    使用此方法打开文件后需要在最后添加关闭文件的操作
+                # finally:
+                #     file.close()
+            with open(file_path, 'a', encoding='utf-8') as file:    #使用此方法打开文件不需要进行关闭文件的操作
+
+                if isinstance(value,dict):
+                    write_data = yaml.dump(value,allow_unicode=True,sort_keys=False)
+                    file.write(write_data)
+                else:
+                    print('写入到【extract】的数据必须为字典类型！！！')
+        except  Exception as e:
+            print(e)
+
+
+
+
+
+if __name__ == '__main__':
+    res = get_testcase_yaml('login.yaml')[0]
+    url = res['baseInfo']['url']
+    new_url = 'http://127.0.0.1:8787//'+url
+    method = res['baseInfo']['method']
+    header = res['baseInfo']['header']
+    data = res['testCase'][0]['data']
+    from sendrequests import SendRequest
+    send = SendRequest()
+    send = send.post(url=new_url,data=data,header=header)
+    print(send)
+
+    token = send.get('token')
+
+    write_data = {}
+    write_data['Token'] = token
+
+    read = ReadyamlData()
+    read.write_yaml_data(write_data)
+
+
+
+    #pyrhon常用的数据类型：str(字符串) list（列表） dict（字典） set（集合） tuple（元祖）
+    #json序列化和反序列化
+    #json序列化，就是将python的字典类型转换成字符串
+    #json反序列化，就是将python的字符串转换成字典
