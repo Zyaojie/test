f0e2cbf4c03da065c6f3de7ba577611d41a15110
Zyaojie <1750724954@qq.com>
2024-12-05 20:20:03 +0800

    apiutil

--- apiutil.py
+++ apiutil.py
@@ -22,17 +22,15 @@ class BaceRequsts:
                 start_index = str_data.index('$')
                 end_index = str_data.index('}',start_index)
                 ref_all_params = str_data[start_index:end_index+1]
-                print(ref_all_params)
                 #取出函数名
                 func_name = ref_all_params[2:ref_all_params.index('(')]
-                print(func_name)
                 #取出函数中的参数值
                 funcs_params = ref_all_params[ref_all_params.index('(')+1:ref_all_params.index(')')]
-                print(funcs_params)
 
                 #传入替换的参数获取对应的值
+                print('yaml文件替换解析前：',str_data)
                 extract_data = getattr(Debugtaik(),func_name)(*funcs_params.split(',') if funcs_params else '')
-                print(extract_data)
+                print('yaml文件替换解析后：',extract_data)
 
 
 
