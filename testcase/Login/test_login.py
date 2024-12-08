ba357b55f90518334631818d8d6858e15c2551cb
Zyaojie <1750724954@qq.com>
2024-12-08 19:44:52 +0800

    装饰器

--- testcase/Login/test_login.py
+++ testcase/Login/test_login.py
@@ -8,21 +8,24 @@ import time
 import pytest
 
 
-@pytest.fixture(scope="",params="",autouse=False,ids="",name="")
-def fixture_test():
+@pytest.fixture(scope="function",autouse=True ,params=['北京', '广州','深圳'],ids=['BJ','GZ','SZ'],name='test3')
+def fixture_test(request):
     '''前后置处理'''
-    print('这是处理前后置的方法')
+    # print('————————————————接口测试开始————————————————')
+    # yield
+    # print('————————————————接口测试结束————————————————')
+    return request.param
 
 class TestLogin:
 
     def test_case01(self):
-        print('我第二个执行')
+        print('用例1')
 
-    @pytest.mark.skip(reason='跳过')
 
     def test_case02(self):
-        print('我第三个执行')
+        print('用例2')
 
 
-    def test_case03(self):
-        print('我第一个执行')
+    def test_case03(self,test3):
+        print('用例3')
+        print('获取前置操作的参数：',test3)
